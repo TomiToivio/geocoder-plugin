06131ef92ed27d1c42f38c1537ed53ea1b2a22ec
TomiToivio <tomi@flossmanuals.net>
2014-08-19 22:18:49 +0300

    Trouble with float

--- geocoder-plugin.php
+++ geocoder-plugin.php
@@ -25,6 +25,8 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+
+/* Geocoder Widget */
 class ttGeocoderWidget extends WP_Widget {
 
   function SeAnalyticsWidget() {
@@ -47,23 +49,52 @@ class ttGeocoderWidget extends WP_Widget {
   }
 }
 
+/* Registering Geocoder Widget */
 function tt_register_geocoder_widget() {
   register_widget( 'ttGeocoderWidget' );
 }
 add_action( 'widgets_init', 'tt_register_geocoder_widget' );
 
+/* Content for Geocoder Widget */
 function tt_geocoder() {
+
+  /* Get IP address from server */
   $ip= $_SERVER['REMOTE_ADDR'];
+
+  /* Get IP address coordinates from Dazzlepod API */
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, "http://dazzlepod.com/ip/" . $ip . ".json");
   curl_setopt($ch, CURLOPT_HEADER, 0);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   $data = curl_exec($ch);
   curl_close($ch);
-  $data = json_decode($data,true);
+  $data = json_decode($data, true);
+  echo var_dump($data);
+
+  /*  $lat = floatval($data['latitude']);
+      $lng = floatval($data['longitude']); */
+
   $lat = $data['latitude'];
   $lng = $data['longitude'];
 
+  echo var_dump($lat);
+  echo var_dump($lng);
+
+  echo number_format($lat,64);
+  echo number_format($lng,64);
+
+
+  /* Echo IP Address */
+  echo '<p>Your IP Address is: ';
+  echo $ip;
+  echo '</p>';
+
+  /* Echo coordinates */
+  echo '<p>Your coordinates: ';
+  echo $lat . ',' . $lng;
+  echo '</p>';
+
+  /* Get formatted address from Geocoder API */
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, "http://api.okf.fi/gis/1/geocode.json?lat=" . $lat . "&lng=" . $lng);
   curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -73,8 +104,14 @@ function tt_geocoder() {
   $data = json_decode($data,true);
   $data=$data['results'];
   $data=$data[0];
-  $data=$data['formatted_address']; 
-  echo $data;
+  $osoite=$data['formatted_address']; 
+
+  /* Echo the address */
+  echo '<p><a href="http://api.okf.fi/console/">Geocoder API</a> result address: ';
+  echo $osoite;
+  echo '</p>';
+
+
 }
 
 ?>
